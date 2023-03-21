466136f1f8e594800a7bf846d6d0a7cfdeba8712
kano-mathiya <128366028+kano-mathiya@users.noreply.github.com>
2023-03-21 15:50:22 +0530

    form

---
+++ new/form.php
@@ -0,0 +1,24 @@
+<html>
+<head></head>
+<table border="2" align="center">
+<form method="POST" action="">
+<tr><td>name:</td><td><input type="text" name="name"> </td></tr> 
+<tr><td>gender:</td><td><input type="radio" name="gender" value="male">male
+<input type="radio" name="gender" value="female">female  </td></tr> 
+<tr><td>addres:</td><td><textarea name="addres">addres</textarea> </td></tr> 
+<tr><td>mobileno:</td><td><input type="number" name="mobileno"></td>	</tr> 
+<tr><td>hob</td><td><input type="checkbox" name="hob[]" value="music">music:<br>
+<input type="checkbox" name="hob[]" value="reading">reading:<br>
+<input type="checkbox" name="hob[]" value="cricket">cricket:<br>
+<input type="checkbox" name="hob[]" value="computer">computer:<br></td></tr>
+<tr><td>email:</td><td><input type="text" name="email"></td></tr>
+<tr><td>password:</td><td><input type="password" name="psw"></td></tr>
+<tr><td colspan="2" align="center"><input type="submit" name="submit" value="submit"></td></tr>
+		</td>
+<tr><td colspan="2" align="center"><input type="reset" name="reset" value="reset"></td></tr>
+		</td>
+		
+</form>
+		</table>
+	</body>
+</html>
